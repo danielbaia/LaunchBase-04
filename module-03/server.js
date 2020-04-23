3dd5d96f30d9eb6271237b3031fcf48e3b4a18dc
Daniel Baia <danielbaia29@gmail.com>
2020-04-23 20:25:49 -0300

    delete module 3 and restart

--- module-03/server.js
+++ module-03/server.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const server = express();
-
-
-server.get("/", function(req, res) {
-    return res.send('H1, how are you')
-});
-
-server.listen(5000, function() {
-    console.log('Server is running!');
-});
+import 'express'
