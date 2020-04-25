8a282051c72683480af2c5b9d4701ed92d43fc7f
Daniel Baia <danielbaia29@gmail.com>
2020-04-25 10:04:46 -0300

    add challenge 03-02

---
+++ challenge-03-02/about.js
@@ -0,0 +1,32 @@
+module.exports = [{
+    url_logo: "https://rocketseat.com.br/static/images/logo-rocketseat.svg",
+    title: "Rocketseat",
+    subtitle: "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
+    description: "No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa.",
+    tecnologies: [{
+            name: "Javascript"
+        },
+        {
+            name: "NodeJS"
+        },
+        {
+            name: "ReactJS"
+        }
+    ],
+
+    links: [{
+            name: "Github",
+            url: "https://github.com/Rocketseat"
+        },
+
+        {
+            name: "Linkedin",
+            url: "https://www.linkedin.com/school/rocketseat/about/"
+        },
+
+        {
+            name: "Twitter",
+            url: "https://twitter.com/rocketseat"
+        }
+    ]
+}]
