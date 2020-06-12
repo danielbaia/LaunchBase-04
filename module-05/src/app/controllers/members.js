8c66bf067c10d1b7317c73df7daa2094210e3146
Daniel Baia <danielbaia29@gmail.com>
2020-06-12 15:30:14 -0300

    create model members

--- module-05/src/app/controllers/members.js
+++ module-05/src/app/controllers/members.js
@@ -1,28 +1,63 @@
+const Members = require('../models/Members');
 const { age, date } = require('../../lib/utils');
 
 module.exports = {
     index(req, res) {
-        return res.render("members/index");
+
+        Members.all(function(members) {
+            return res.render("members/index", { members });
+        })
+
     },
     create(req, res) {
-        return res.render('members/create');
+        return res.render("members/create");
+
     },
     post(req, res) {
+
+
         const keys = Object.keys(req.body);
 
         for (const key of keys) {
-            if (req.body[key] == "")
-                return res.send("Please, fill all the fields!");
+            if (req.body[key] == "") {
+                return res.render("Please, fill all the fields");
+            }
         }
-        return;
+
+        Members.create(req.body, function(member) {
+            return res.redirect(`members/${member.id}`);
+        })
+
     },
     show(req, res) {
-        return;
+        Members.find(req.params.id, function(member) {
+
+            if (!member) res.send("Member not found...");
+
+            member.birth = date(member.birth).format;
+            member.created_at = date(member.created_at).format;
+
+            res.render(`members/show`, { member });
+        })
+
     },
     edit(req, res) {
-        return;
+
+        Members.find(req.params.id, function(member) {
+
+            if (!member) res.send("Member not found...");
+
+            member.birth = date(member.birth).iso;
+            member.created_at = date(member.created_at).format;
+
+            res.render(`members/edit`, { member });
+        })
+
     },
+
     put(req, res) {
+
+
         const keys = Object.keys(req.body);
 
         for (const key of keys) {
@@ -30,7 +65,17 @@ module.exports = {
                 return res.send("Please, fill all the fields!");
         }
 
-        return;
+        Members.update(req.body, function() {
+            res.redirect(`members/${req.body.id}`);
+        })
+
     },
-    delete(req, res) { return; }
+
+    delete(req, res) {
+
+        Members.delete(req.body.id, function() {
+            res.redirect(`/members`)
+        })
+    }
+
 }
